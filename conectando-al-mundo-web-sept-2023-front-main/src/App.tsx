d00f6eb1ca6a96efa821b87ee726fbf6646f4f8c
Rosa Palaci <palacibelrose431@gmail.com>
2023-09-14 15:56:57 -0600

    listo pao

--- conectando-al-mundo-web-sept-2023-front-main/src/App.tsx
+++ conectando-al-mundo-web-sept-2023-front-main/src/App.tsx
@@ -3,8 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Example from "./components/Example";
 import Principal from "./models/Principal";
-import Registro from "./models/Resgistro";
-import Registro from "./models/Calendar";
+import Registro from "./models/Registro";
+import Calendar from "./models/Calendar";
 import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
