1215a6e526229b4e046781d6184dc8c319479d9b
Rosa Palaci <palacibelrose431@gmail.com>
2023-09-14 16:25:06 -0600

    depurando principal

--- conectando-al-mundo-web-sept-2023-front-main/src/models/Principal.tsx
+++ conectando-al-mundo-web-sept-2023-front-main/src/models/Principal.tsx
@@ -6,43 +6,11 @@ import { Pokemon } from "./Pokemon";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function Principal() {
-  <h1 className="Title">Things to do</h1>;
-}
-
 function Pokedex() {
-  const columnSize = 6;
-  const [pokemonId, setPokemonId] = useState(0);
-  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-  function selectPokemonId(id: number) {
-    setPokemonId(id);
-  }
-
-  function getPokemon() {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
-      .then((response) => {
-        setPokemon(response.data);
-      });
-  }
-
-  useEffect(() => {
-    console.log("Cambio el valor de pokemonId y ahora es " + pokemonId);
-  }, [pokemonId]);
-
   return (
     <div className="Pokedex">
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={columnSize} className="Busqueda">
-          <Busqueda
-            searchPokemon={getPokemon}
-            changePokemonId={selectPokemonId}
-          ></Busqueda>
-        </Grid>
-        <Grid item xs={columnSize} className="Resultado">
-          {pokemon && <PokemonInfo pokemon={pokemon}></PokemonInfo>}
-        </Grid>
-      </Grid>
+      <h1>Things to do</h1>
+      <h2>Title: {}</h2>
     </div>
   );
 }
